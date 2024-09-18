86912edcf5f4148bdb01ef53896a60a5d303ddcf
Bobby Davidson <bobbyjames839@gmail.com>
2024-09-18 10:53:49 +0100

    fnial

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -9,26 +9,29 @@ export const Footer = () => {
         <div className='footer'>
             <div className='footer_main'>
 
-                <div className='footer_section'>
+                <div className='footer_section footer_section_right'>
                     <span className="footer_logo">Logo</span>
                 </div>
 
-                <div className='footer_section'>
-                    <p className='footer_title'>Find us on social</p>
-                    {socialLinks.map((social, index) => (
-                        <div className='footer_social' key={index}>
-                            <FontAwesomeIcon icon={faArrowUpRightFromSquare} className='footer_icon'/> 
-                            <a href='/' className='footer_text'>{social}</a>
-                        </div>
-                    ))}
-                </div>
+                <div className='footer_main_inner'>
+                    <div className='footer_section'>
+                        <p className='footer_title'>Find us on social</p>
+                        {socialLinks.map((social, index) => (
+                            <div className='footer_social' key={index}>
+                                <FontAwesomeIcon icon={faArrowUpRightFromSquare} className='footer_icon'/> 
+                                <a href='/' className='footer_text'>{social}</a>
+                            </div>
+                        ))}
+                    </div>
 
-                <div className='footer_section'>
-                    <p className='footer_title'>Contact Us</p>
-                    <p className='footer_text'>info@company.com</p>
-                    <p className='footer_text'>+44 0000 000000</p>
-                    <p className='footer_text'>123 Company St, City, Country</p>
+                    <div className='footer_section'>
+                        <p className='footer_title'>Contact Us</p>
+                        <p className='footer_text'>info@company.com</p>
+                        <p className='footer_text'>+44 0000 000000</p>
+                        <p className='footer_text'>123 Company St, City, Country</p>
+                    </div>
                 </div>
+                
 
             </div>
             <p className='footer_copy'>© 2024 B&T Bookings. All rights reserved.</p>
